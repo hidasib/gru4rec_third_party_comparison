dd3212a6fcfacb3cf5e14fb495965a16c3f2ef2e
czappa <czappadam@gmail.com>
2023-04-20 18:59:02 +0000

    minor changes in coveo preproc

--- coveo_preproc.py
+++ coveo_preproc.py
@@ -4,33 +4,62 @@ import gc
 import datetime as dt
 import glob
 import argparse
+import os
 
-def main(path:str):
-    data = pd.read_csv(f'{path}/release_10_23_2020.csv', sep=',', date_parser=['server_timestamp_epoch_ms'], dtype={'session_id_hash':str, 'event_type':'category', 'product_action':str, 'product_skus_hash':str, 'hashed_url':str})
+
+def main(path: str):
+    data = pd.read_csv(
+        os.path.join(path, "release_10_23_2020.csv"),
+        sep=",",
+        date_parser=["server_timestamp_epoch_ms"],
+        dtype={
+            "session_id_hash": str,
+            "event_type": "category",
+            "product_action": str,
+            "product_skus_hash": str,
+            "hashed_url": str,
+        },
+    )
+    
     session_hashes = data.session_id_hash.unique()
     item_hashes = data.product_skus_hash.unique()
-
-    session_hash_start = data.groupby('session_id_hash', as_index=False).agg(Start=('server_timestamp_epoch_ms', 'min'))
-    session_hash_start.sort_values(by=['Start'], inplace=True)
-    session_hash_start['session_idx'] = np.arange(session_hash_start.session_id_hash.nunique())
-
-    data = pd.merge(data, session_hash_start, on=['session_id_hash'])
-    data = pd.merge(data, pd.DataFrame({'product_skus_hash': item_hashes, 'item_id': np.arange(len(item_hashes))}))
-    data = data.sort_values(['session_idx', 'server_timestamp_epoch_ms', 'item_id'])
+    data = pd.merge(
+        data,
+        pd.DataFrame(
+            {"session_id_hash": session_hashes, "session_idx": np.arange(len(session_hashes))}
+        ),
+    )
+    data = pd.merge(
+        data,
+        pd.DataFrame(
+            {"product_skus_hash": item_hashes, "item_id": np.arange(len(item_hashes))}
+        ),
+    )
+    data = data.sort_values(["session_idx", "server_timestamp_epoch_ms", "item_id"])
 
     data["session_id"] = np.cumsum(
         np.hstack(
             [
                 False,
-                (data.server_timestamp_epoch_ms.values[1:] - data.server_timestamp_epoch_ms.values[:-1] > 3600 * 1000)
+                (
+                    data.server_timestamp_epoch_ms.values[1:]
+                    - data.server_timestamp_epoch_ms.values[:-1]
+                    > 3600 * 1000
+                )
                 | (data.session_idx.values[1:] != data.session_idx.values[:-1]),
             ]
         )
     )
 
-    view = data[data.product_action == 'detail']
-    view = pd.DataFrame({'session_id': view.session_id, 'time': view.server_timestamp_epoch_ms, 'item_id': view.item_id})
-    view = view.sort_values(by=['session_id', 'time', 'item_id'])
+    view = data[data.product_action == "detail"]
+    view = pd.DataFrame(
+        {
+            "session_id": view.session_id,
+            "time": view.server_timestamp_epoch_ms,
+            "item_id": view.item_id,
+        }
+    )
+    view = view.sort_values(by=["session_id", "time", "item_id"])
     view.reset_index(inplace=True, drop=True)
 
     dupmask = np.hstack(
@@ -63,7 +92,8 @@ def main(path:str):
         )
         isupp = view_dedup.groupby("item_id").size()
         view_dedup.drop(
-            view_dedup[view_dedup.item_id.isin(isupp[isupp < 5].index)].index, inplace=True
+            view_dedup[view_dedup.item_id.isin(isupp[isupp < 5].index)].index,
+            inplace=True,
         )
         gc.collect()
         print(
@@ -71,7 +101,7 @@ def main(path:str):
             view_dedup.session_id.nunique(),
             view_dedup.item_id.nunique(),
         )
-        
+
     processed = pd.DataFrame(
         {
             "SessionId": view_dedup.session_id.values,
@@ -86,7 +116,9 @@ def main(path:str):
     )
 
     sbeg = processed.groupby("SessionId").Time.min()
-    tsplit = 1545782400 * 1000# this is the end of the last full day
+    tsplit = (
+        processed.Time.max()
+    )  # 1545782400 * 1000  # this is the end of the last full day
     tday = 86400 * 1000
     test = processed[processed.SessionId.isin(sbeg[sbeg >= tsplit - tday].index)]
     train = processed[processed.Time < tsplit - tday]
@@ -134,9 +166,7 @@ def main(path:str):
     item_view_avg = []
     diff_min = []
     diff_max = []
-    for curr_path in glob.glob(
-        f"{path}/coveo_processed_view*"
-    ):
+    for curr_path in glob.glob(f"{path}/coveo_processed_view*"):
         data = pd.read_csv(curr_path, sep="\t", dtype={"ItemId": "str"})
         data.Time = data.Time
         names.append(curr_path.split("/")[-1])
@@ -145,14 +175,20 @@ def main(path:str):
         num_items.append(data.ItemId.nunique())
         num_days.append((data.Time.max() - data.Time.min()) / tday)
         start_times.append(
-            dt.datetime.utcfromtimestamp(data.Time.min() / 1000).strftime("%Y-%m-%d %H:%M:%S.%f")
+            dt.datetime.utcfromtimestamp(data.Time.min() / 1000).strftime(
+                "%Y-%m-%d %H:%M:%S.%f"
+            )
         )
         end_times.append(
-            dt.datetime.utcfromtimestamp(data.Time.max() / 1000).strftime("%Y-%m-%d %H:%M:%S.%f")
+            dt.datetime.utcfromtimestamp(data.Time.max() / 1000).strftime(
+                "%Y-%m-%d %H:%M:%S.%f"
+            )
         )
         slength = data.groupby("SessionId").size()
         itemview = data.groupby("ItemId").size()
-        sdiff = data.groupby("SessionId").Time.max() - data.groupby("SessionId").Time.min()
+        sdiff = (
+            data.groupby("SessionId").Time.max() - data.groupby("SessionId").Time.min()
+        )
         sdiff = sdiff / 1000
         length_min.append(slength.min())
         length_max.append(slength.max())
@@ -182,11 +218,18 @@ def main(path:str):
         f"{path}/stats.tsv",
         sep="\t",
         index=False,
-        float_format='%.4f',
+        float_format="%.4f",
     )
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--path", type=str, help="Path of the directory of the dataset", required=True)
+    parser.add_argument(
+        "-p",
+        "--path",
+        type=str,
+        help="Path of the directory of the dataset",
+        required=True,
+    )
     args = parser.parse_args()
     main(path=args.path)
